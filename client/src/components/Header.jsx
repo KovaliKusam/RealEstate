f2f21984f571999bb8bcef3806571018ad94d17f
kovali.kusam <kovali.kusam@tectoro.com>
2024-11-06 12:49:15 +0530

    Changed a bit of header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,19 +13,20 @@ const Header = () => {
                 </h1>
             </Link>
             
-            <ul className="flex gap-4">
+            <ul className="flex gap-10">
                 <Link to="/">
                     <li className="hidden sm:inline text-slate-700 hover:underline"> Home </li>
-                </Link>
-                <Link to="/about">
-                    <li className="hidden sm:inline text-slate-700 hover:underline"> About </li>
                 </Link>
                 <Link to='/sign-in'>
                     <li className="text-slate-700 hover:underline"> Sign In </li>
                 </Link>
+                <Link to="/about">
+                    <li className="hidden sm:inline text-slate-700 hover:underline"> About </li>
+                </Link>
+                
             </ul>
             <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <input type='text' placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
                 <FaSearch className="text-slate-600"/>
             </form>
         </div>
